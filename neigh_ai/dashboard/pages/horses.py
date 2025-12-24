9aa35f0849c48a61e97b988e3759085141c24580
Hayden Sather <hayden@Haydens-MacBook-Air.local>
2025-12-24 01:11:57 -0800

    feat: Clean

--- neigh_ai/dashboard/pages/horses.py
+++ neigh_ai/dashboard/pages/horses.py
@@ -70,7 +70,7 @@ def plot_race_scatter(df: pd.DataFrame, horse_racing_api_id: str, model) -> None
             y=y_pred,
             mode="lines",
             line={"color": "white", "width": 4},
-            name="Model prediction",
+            name="Average Speed",
         )
     )
     fig.update_layout(
@@ -107,7 +107,7 @@ def plot_hist(df: pd.DataFrame, column: str, horse_id: str, title: str) -> None:
     fig.update_layout(xaxis_title=column, yaxis_title="Count", bargap=0.1)
 
     # Display in Streamlit
-    with st.expander(f"{title}: {percentileofscore(df[column], df.iloc[idx][column], kind='rank'):.2f}th percentile"):
+    with st.expander(f"{title}: {percentileofscore(df[column], df.iloc[idx][column], kind='rank'):.0f}%"):
         st.plotly_chart(fig, width="stretch")
 
 
@@ -129,7 +129,7 @@ if rows:
     col1, col2 = st.columns(2)
 
     col1.metric(
-        "Race score Percentile", f"{percentileofscore(df['race_score'], horse_df['race_score'], kind='rank'):.2f}%"
+        "Race Score Percentile", f"{percentileofscore(df['race_score'], horse_df['race_score'], kind='rank'):.1f}%"
     )
     col1.metric("Num races", df.iloc[idx]["num_races"])
 
@@ -137,79 +137,67 @@ if rows:
     col2.metric("Avg winnings per race", f"${int(np.exp(horse_df['log_avg_prize_money'])):,}")
 
     fig = plot_race_scatter(df=race_df, horse_racing_api_id=horse_df["horse_racing_api_id"], model=race_model.model)
+
+    st.subheader("Performance Features (by percentile and distribution)")
     plot_hist(df, column="avg_speed_diff", horse_id=horse_df["horse_racing_api_id"], title="Speed")
     plot_hist(df, column="log_avg_prize_money", horse_id=horse_df["horse_racing_api_id"], title="Money per race")
     plot_hist(df, column="avg_g1_finish", horse_id=horse_df["horse_racing_api_id"], title="Average G1 Finish")
     plot_hist(df, column="avg_g2_finish", horse_id=horse_df["horse_racing_api_id"], title="Average G2 Finish")
     plot_hist(df, column="avg_g3_finish", horse_id=horse_df["horse_racing_api_id"], title="Average G3 Finish")
 
+    # Pedigree charts
+    st.subheader("Pedigree Scores (by percentile and distribution)")
+
+    def pedigree_charts(pedigree_df: pd.DataFrame, column: str) -> None:
+        clean_name: str = column.replace("_", " ").title()
+        if not len(pedigree_df):
+            return
+        with st.expander(
+            f"{clean_name}: Avg={percentileofscore(df['race_score'], df.iloc[idx][f'avg_{column}'], kind='rank'):.0f}%, Max={percentileofscore(df['race_score'], df.iloc[idx][f'max_{column}'], kind='rank'):.0f}%"
+        ):
+            st.subheader(clean_name)
+            st.text(f"Max race score: {df.iloc[idx][f'max_{column}']}")
+            st.text(f"Avg race score: {df.iloc[idx][f'avg_{column}']}")
+            st.text(f"Avg race score: {np.mean(pedigree_df['race_score'])}")
+            assert np.isclose(df.iloc[idx][f"avg_{column}"], np.mean(pedigree_df["race_score"]))
+            st.dataframe(pedigree_df, width="stretch", key=column, hide_index=True)
+
     # Sire Siblings
-    st.subheader("Sire Siblings")
-    pedigree_df = df[
-        (df["sire_id"] == df.iloc[idx]["sire_id"]) & (df["horse_racing_api_id"] != df.iloc[idx]["horse_racing_api_id"])
-    ]
-    st.text(f"Max race score: {df.iloc[idx]['max_sire_sibling_score']}")
-    st.text(f"Avg race score: {df.iloc[idx]['avg_sire_sibling_score']}")
-    st.dataframe(pedigree_df, width="stretch", key="sibling_table", hide_index=True)
-
+    pedigree_charts(
+        df[
+            (df["sire_id"] == df.iloc[idx]["sire_id"])
+            & (df["horse_racing_api_id"] != df.iloc[idx]["horse_racing_api_id"])
+        ],
+        "sire_sibling_score",
+    )
     # Siredam Cousins
-    pedigree_df = df[
-        (df["siredam_id"] == df.iloc[idx]["siredam_id"])
-        & (df["horse_racing_api_id"] != df.iloc[idx]["horse_racing_api_id"])
-    ]
-    if len(pedigree_df):
-        st.subheader("Siredam Cousins")
-        st.text(f"Max race score: {df.iloc[idx]['max_siredam_cousin_score']}")
-        st.text(f"Avg race score: {df.iloc[idx]['avg_siredam_cousin_score']}")
-        st.dataframe(pedigree_df, width="stretch", key="siredam_cousin_table", hide_index=True)
-
+    pedigree_charts(
+        df[
+            (df["siredam_id"] == df.iloc[idx]["siredam_id"])
+            & (df["horse_racing_api_id"] != df.iloc[idx]["horse_racing_api_id"])
+        ],
+        "siredam_cousin_score",
+    )
     # Siresire Cousins
-    pedigree_df = df[
-        (df["siresire_id"] == df.iloc[idx]["siresire_id"])
-        & (df["horse_racing_api_id"] != df.iloc[idx]["horse_racing_api_id"])
-    ]
-    if len(pedigree_df):
-        st.subheader("Siresire Cousins")
-        st.text(f"Max race score: {df.iloc[idx]['max_siresire_cousin_score']}")
-        st.text(f"Avg race score: {df.iloc[idx]['avg_siresire_cousin_score']}")
-        assert np.isclose(df.iloc[idx]["avg_siresire_cousin_score"], np.mean(pedigree_df["race_score"]))
-        st.dataframe(pedigree_df, width="stretch", key="siresire_cousin_table", hide_index=True)
+    pedigree_charts(
+        df[
+            (df["siresire_id"] == df.iloc[idx]["siresire_id"])
+            & (df["horse_racing_api_id"] != df.iloc[idx]["horse_racing_api_id"])
+        ],
+        "siresire_cousin_score",
+    )
 
     # Siresire Aunt/Uncle
-    pedigree_df = df[df["sire_id"] == df.iloc[idx]["siresire_id"]]
-    if len(pedigree_df):
-        st.subheader("Siresire Aunt/Uncle")
-        st.text(f"Max race score: {df.iloc[idx]['max_siresire_auntuncle_score']}")
-        st.text(f"Avg race score: {df.iloc[idx]['avg_siresire_auntuncle_score']}")
-        assert np.isclose(df.iloc[idx]["avg_siresire_auntuncle_score"], np.mean(pedigree_df["race_score"]))
-        st.dataframe(pedigree_df, width="stretch", key="siresire_auntuncle_table", hide_index=True)
+    pedigree_charts(df[df["sire_id"] == df.iloc[idx]["siresire_id"]], "siresire_auntuncle_score")
 
     # Siredam Aunt/Uncle
-    pedigree_df = df[df["dam_id"] == df.iloc[idx]["siredam_id"]]
-    if len(pedigree_df):
-        st.subheader("Siredam Aunt/Uncle")
-        st.text(f"Max race score: {df.iloc[idx]['max_siredam_auntuncle_score']}")
-        st.text(f"Avg race score: {df.iloc[idx]['avg_siredam_auntuncle_score']}")
-        assert np.isclose(df.iloc[idx]["avg_siredam_auntuncle_score"], np.mean(pedigree_df["race_score"]))
-        st.dataframe(pedigree_df, width="stretch", key="siredam_auntuncle_table", hide_index=True)
+    pedigree_charts(df[df["dam_id"] == df.iloc[idx]["siredam_id"]], "siredam_auntuncle_score")
 
     # Damdam Aunt/Uncle
-    pedigree_df = df[df["dam_id"] == df.iloc[idx]["damdam_id"]]
-    if len(pedigree_df):
-        st.subheader("Damdam Aunt/Uncle")
-        st.text(f"Max race score: {df.iloc[idx]['max_damdam_auntuncle_score']}")
-        st.text(f"Avg race score: {df.iloc[idx]['avg_damdam_auntuncle_score']}")
-        assert np.isclose(df.iloc[idx]["avg_damdam_auntuncle_score"], np.mean(pedigree_df["race_score"]))
-        st.dataframe(pedigree_df, width="stretch", key="damdam_auntuncle_table", hide_index=True)
+    pedigree_charts(df[df["dam_id"] == df.iloc[idx]["damdam_id"]], "damdam_auntuncle_score")
 
     # Damsire Aunt/Uncle
-    pedigree_df = df[df["sire_id"] == df.iloc[idx]["damsire_id"]]
-    if len(pedigree_df):
-        st.subheader("Damsire Aunt/Uncle")
-        st.text(f"Max race score: {df.iloc[idx]['max_damsire_auntuncle_score']}")
-        st.text(f"Avg race score: {df.iloc[idx]['avg_damsire_auntuncle_score']}")
-        assert np.isclose(df.iloc[idx]["avg_damsire_auntuncle_score"], np.mean(pedigree_df["race_score"]))
-        st.dataframe(pedigree_df, width="stretch", key="damsire_auntuncle_table", hide_index=True)
+    pedigree_charts(df[df["sire_id"] == df.iloc[idx]["damsire_id"]], "damsire_auntuncle_score")
 
 else:
     st.write("_Click a row above to see details here_")
